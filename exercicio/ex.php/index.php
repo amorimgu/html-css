7aca5cb261c03e3a70d5e0f8b92a38a05ffc63d6
Gustavo Amorim <gustavoamorim@qualitysmi.com.br>
2022-08-16 17:12:07 -0300

    Exercicio novo de PHP

---
+++ exercicio/ex.php/index.php
@@ -0,0 +1,51 @@
+<?php 
+
+$nome = "Eu sou um texto"; // Textos precisam ficar dentro de áspas
+$idade = 30; //comentário - Para número não se usa aspas
+
+$listadefrutas = array(
+    "uva",
+    "abacaxi",
+);
+
+$a = 5;
+$a = 10;
+$a = 15; // Sempre vai prevalecer á última atribuição
+
+echo $a;
+
+$frutas = array(  
+    "Frutas_Vermelhas" => array()
+    "Maçã",  
+    "Banana",
+    "Uva",
+    "abacaxi",
+    "Melancia",
+    "Crambola",
+    "Jacaboticaba"
+);
+
+echo $frutas;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <?php
+            $titulo_pagina = "Organização"
+
+            $frutas = $Array(
+
+                
+            )
+        ?>
+    </main>
+</body>
+</html>
